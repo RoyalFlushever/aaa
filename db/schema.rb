08d2b9e4135c0a7394c09bf2ab365b581ccfa16c
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-10-22 21:41:49 -0500

    devise confirmable and lockable strategy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_011509) do
+ActiveRecord::Schema.define(version: 2018_10_23_023914) do
 
   create_table "sessions", force: :cascade do |t|
     t.string "session_id", null: false
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 2018_10_23_011509) do
     t.datetime "updated_at", null: false
     t.string "provider", limit: 50, default: "", null: false
     t.string "uid", limit: 500, default: "", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.datetime "locked_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
