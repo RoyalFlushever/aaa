9365a71408a9bfc327d7a7c3cc4a378294175997
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-10-22 21:15:37 -0500

    add facebook callback

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -1,2 +1,21 @@
 class Users::OmniauthController < ApplicationController
+  # facebook callback
+  def facebook
+    # create a user from auth data
+    @user = User.create_from_provider_data(request.env['omniauth.auth'])
+    
+    # user created successfully 
+    if @user.persisted?
+      # redirect to user
+      sign_in_and_redirect @user
+      # set flash success message ( devise controller method ) - refer devise.en.yml
+      set_flash_message( :notice, :success, kind: 'Facebook') if is_navigational_format?
+    
+    # failed a new user creation  
+    else
+      flash[:error] = "There was a problem signing you through Facebook. Please try later."
+      redirect_to new_user_registration_path
+    end
+
+  end
 end
