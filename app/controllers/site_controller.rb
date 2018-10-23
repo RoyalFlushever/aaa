ad71c178ae851c0d1e29126dd86f0ac102442031
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-10-23 09:30:54 -0500

    add privacy, tos page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,11 @@
 class SiteController < ApplicationController
   def landing
   end
+
+  def privacy
+  end
+
+  def tos
+      
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'site#landing'
+  get '/privacy', to: 'site#privacy', as: :privacy
+  get '/tos', to: 'site#tos', as: :tos
 end
