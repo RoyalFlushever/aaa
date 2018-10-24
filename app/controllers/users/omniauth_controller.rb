96825e44990a965b9ded329e53dae8b816a5136c
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-10-24 15:13:05 -0500

    Update app/controllers/users/omniauth_controller.rb

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -13,14 +13,14 @@ class Users::OmniauthController < Devise::OmniauthCallbacksController
     # create a user from auth data
     @user = User.create_from_provider_data(request.env['omniauth.auth'])
     
-    # user created successfully 
+    # user created successfully
     if @user.persisted?
       # redirect to user
       sign_in_and_redirect @user
       # set flash success message ( devise controller method ) - refer devise.en.yml
       set_flash_message( :notice, :success, kind: kind) if is_navigational_format?
     
-    # failed a new user creation  
+    # failed a new user creation
     else
       flash[:error] = "There was a problem signing you through #{kind}. Please try later."
       redirect_to new_user_registration_path
@@ -28,8 +28,10 @@ class Users::OmniauthController < Devise::OmniauthCallbacksController
 
   end
 
+  # callback failure
   def failure
     flash[:error] = 'There was a problem signing you in. Please register or try signing in later.' 
     redirect_to new_user_registration_url
   end
+  
 end
