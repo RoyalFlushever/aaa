13461b8ad7a148ac504690aab829a990af652b22
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-10-29 21:49:25 -0500

    remote input hover

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   devise :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
-  after_create :notify_admin
+  # after_create :notify_admin
 
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
