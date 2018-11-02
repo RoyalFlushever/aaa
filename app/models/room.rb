8939ceea55647191b5f7cf73cb34a05e8c6a93c1
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-11-01 22:08:31 -0500

    add validation and default to room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,3 @@
 class Room < ApplicationRecord
+  validates: :name, :home_type, :room_type, :home_type, :accommodate, presence: true
 end
--- db/migrate/20181102025051_create_rooms.rb
+++ db/migrate/20181102025051_create_rooms.rb
@@ -1,17 +1,17 @@
 class CreateRooms < ActiveRecord::Migration[5.2]
   def change
     create_table :rooms do |t|
-      t.integer :home_type
-      t.integer :room_type
+      t.integer :home_type, default: 0
+      t.integer :room_type, default: 0
       t.integer :accommodate
       t.integer :bedroom
       t.integer :bathroom
-      t.decimal :price
+      t.decimal :price, null: false
       t.text :summary
       t.string :name
       t.float :latitude
       t.float :longitude
-      t.boolean :active
+      t.boolean :active, default: true
 
       t.timestamps
     end
