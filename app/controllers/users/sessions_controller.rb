681e13f69d9353a7e0ae993723d393f2c3887fc3
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-11-04 22:08:39 -0600

    devise remeber_for 2 weeks

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,6 @@ module Users
   class SessionsController < Devise::SessionsController
     def new
       self.resource = resource_class.new(sign_in_params)
-      puts "Session Controller #{params.inspect}"
       store_location_for(resource, params[:redirect_to])
       super
     end
