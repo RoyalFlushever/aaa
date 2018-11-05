b14765f8281330ad734754d40485bf199ccd463b
RoyalFlushever <royalflushdiamond918@gmail.com>
2018-11-04 22:02:30 -0600

    fix typos app/models/user.rb

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,8 +2,8 @@ class Room < ApplicationRecord
 
   ROOM_TYPES = ["shared", "entire", "private"].freeze
   
-  validates: :name, :home_type, :room_type, :home_type, :accommodate, presence: true
-  validates: :room_type, inclusion: { in: ROOM_TYPES, message: "%{value} is not valid %{model} type."
+  validates :name, :home_type, :room_type, :home_type, :accommodate, presence: true
+  validates :room_type, inclusion: { in: ROOM_TYPES, message: "%{value} is not valid %{model} type." }
 
   belongs_to :user
 
