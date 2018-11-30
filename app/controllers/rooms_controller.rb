aa5b52b73a46310c8b33df0c3b6e114e9fff4086
RoyalFlushever <blakurah@gmail.com>
2018-11-30 12:19:23 -0600

    update git

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,13 +1,39 @@
 class RoomsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:index, :new]
+  before_action :authenticate_user!, only: [:index, :new, :create]
 
+  # GET /rooms
   def index
     @rooms = Room.all
   end
 
+  # GET /rooms/:id
+  def show
+    
+  end
+
+  # GET /rooms/new
   def new
     @room = Room.new
   end
+
+  # POST /rooms
+  def create
+    @room = Room.new(room_params)
+
+    respond_to do |format|
+      if @room.save
+        format.html { redirect_to @room, notice: 'Room was successfully created.'}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
+  private
+    def room_params
+      params.require(:room).permit(:room_type, :home_type, :accommodate, :bedroom, :bathroom)
+    end
 
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
 
   ROOM_TYPES = ["shared", "entire", "private"].freeze
-  HOME_TYPES = %w(house, apartment, bed&breakfast)
+  HOME_TYPES = %w(house apartment bed&breakfast)
   
   validates :home_type, :room_type, :accommodate, :bedroom, :bathroom, presence: true
   validates :room_type, inclusion: { in: ROOM_TYPES, message: "%{value} is not valid %{model} type." }
